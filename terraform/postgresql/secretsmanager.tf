7444d7793318a96073b72edb9f46e9be90398155
innossh <innossh@users.noreply.github.com>
2019-07-06 16:50:48 +0900

    Store Redash env vars into Secrets Manager

--- terraform/postgresql/secretsmanager.tf
+++ terraform/postgresql/secretsmanager.tf
@@ -6,8 +6,35 @@ locals {
       name          = "${var.site_id}-fargate/redash-db-url"
       description   = "The Redash database URL including password"
       secret_string = "postgresql://redash:${random_string.role_redash_password.result}@${data.aws_db_instance.fargate_pg.address}/redash"
+    },
+    {
+      name          = "${var.site_id}-fargate/redash-redis-url"
+      description   = "The Redash redis URL"
+      secret_string = "redis://${aws_elasticache_replication_group.fargate_redis.primary_endpoint_address}:6379/0"
+    },
+    {
+      name          = "${var.site_id}-fargate/redash-cookie-secret"
+      description   = "The value of REDASH_COOKIE_SECRET"
+      secret_string = "${random_string.redash_cookie_secret.result}"
+    },
+    {
+      name          = "${var.site_id}-fargate/redash-secret-key"
+      description   = "The value of REDASH_SECRET_KEY"
+      secret_string = "${random_string.redash_secret_key.result}"
     }
   ]
+}
+
+data "aws_elasticache_replication_group" "fargate_redis" {
+  replication_group_id = "${var.site_id}-fargate-redis"
+}
+resource "random_string" "redash_cookie_secret" {
+  length  = 32
+  special = false
+}
+resource "random_string" "redash_secret_key" {
+  length  = 32
+  special = false
 }
 
 resource "aws_secretsmanager_secret" "fargate_redash" {
